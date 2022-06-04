81937d2e6a6edfee44a56465e702dd0f56ad7ad4
VastraKai <fuck@off.now>
2022-06-03 20:28:05 -0400

    Add project files.

---
+++ Logging.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+
+public class Logging
+{
+    // could be used by other things too
+    public string ESC = "\x001B"; // Escape character
+    /// <summary>
+    /// escColor: Gets the escape code to set a foreground color, or reset the foreground color.
+    /// </summary>
+    /// <param name="r">The value to be used for Red</param>
+    /// <param name="g">The value to be used for Green</param>
+    /// <param name="b">The value to be used for Blue</param>
+    /// <returns>Foreground color escape code as string</returns>
+    public string escColor(string? r = null, string? g = null, string? b = null) // returns a foreground color escape code 
+    {                                                                            // (changes the foreground color based on red, green, and blue values)
+        if (r != null && g != null && b != null)
+        {
+            return $"{ESC}[38;2;{r};{g};{b}m";
+        }
+        else
+        {
+            return $"{ESC}[0m";
+        }
+    }
+    /// <summary>
+    /// logWrite: Writes to the screen with formatting
+    /// </summary>
+    /// <param name="str">The string to output to the screen</param>
+    /// <param name="logtype">The log type to use for the message. (E.x. "INFO", "WARN", etc.)</param>
+    public void logWrite(string str, string logtype = "INFO")   // self explanatory  
+    {
+        str = str.Replace("\n", "\n" + escColor("0", "255", "0") + DateTime.Now +
+            escColor("0", "150", "255") + ": " +
+            escColor("255", "145", "0") + logtype.ToUpper() +
+            escColor("0", "255", "100") + " | " + escColor()); // so that new lines in the same don't lack the formatting
+        Console.WriteLine(
+            escColor("0", "255", "0") + DateTime.Now +
+            escColor("0", "150", "255") + ": " +
+            escColor("255", "145", "0") + logtype.ToUpper() +
+            escColor("0", "255", "100") + " | " +
+            escColor() + str);
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using Discord;
+using Discord.Webhook;
+
+class Program
+{
+    [DllImport("user32.dll")]
+    static extern IntPtr GetForegroundWindow();
+
+    [DllImport("user32.dll")]
+    static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+    private string? activeWindowTitle;
+    private string? GetActiveWindowTitle()
+    {
+        const int nChars = 256;
+        StringBuilder Buff = new StringBuilder(nChars);
+        IntPtr handle = GetForegroundWindow();
+
+        if (GetWindowText(handle, Buff, nChars) > 0)
+        {
+            return Buff.ToString();
+        }
+        return null;
+    }
+    public static void Main(string[] args)
+    {
+        Console.CancelKeyPress += new Program().Console_CancelKeyPress;
+        AppDomain.CurrentDomain.ProcessExit += new Program().AppDomain_ProcessExit;
+        Console.Title = "BdCrashRestart";
+        new Program().Run(args);
+    }
+
+    private void AppDomain_ProcessExit(object? sender, EventArgs e)
+    {
+        log.logWrite("Exiting!");
+        Process.GetCurrentProcess().Kill();
+    }
+
+    private void Console_CancelKeyPress(object? sender, ConsoleCancelEventArgs e)
+    {
+        log.logWrite("Exiting!");
+        Process.GetCurrentProcess().Kill();
+    }
+
+    Logging log = new Logging();
+
+    public string? ActiveWindowTitle { get => activeWindowTitle; set => activeWindowTitle = value; }
+
+    public void Run(string[] args)
+    {
+        string DiscordPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Discord\\Update.exe"; // Change as needed
+        string DiscordProcessName = "discord"; // Change as needed, don't append .exe to it
+        string DiscordArgs = $"--processStart {DiscordProcessName}.exe"; // 
+        log.logWrite(log.escColor("0", "150", "255") + "Initialized successfully.");
+        while (true)
+        {
+            string? wndTitle = GetActiveWindowTitle();
+            if(wndTitle != null)
+            {
+                if (wndTitle.ToLower().Contains("betterdiscord crashed")) // checks if the foreground window title contains "betterdiscord crashed"s
+                {
+                    log.logWrite(log.escColor("255", "0", "0") + "BetterDiscord has crashed!\n" + log.escColor("255", "145", "0") + "Restarting discord");
+                    Process[] procs = Process.GetProcessesByName(DiscordProcessName);
+                    log.logWrite("Killing processes");
+                    foreach (Process proc in procs)
+                    {
+                        log.logWrite($"Killing process id {proc.Id}, SessionId {proc.SessionId}");
+                        proc.Kill();
+                    }
+                    log.logWrite("Starting Discord");
+                    Process.Start(DiscordPath, DiscordArgs);
+                    log.logWrite("Done");
+                }
+            }
+            Thread.Sleep(1000);
+        }
+    }
+}
